7d64a8628a9a9902889f7c19a20051d8b793aa07
Jeff Jacobson-Swartfager <jeffja@gmail.com>
2013-03-28 11:43:02 -0700

    initial attempt at making valid api request

--- static/js/load-public-courses.js
+++ static/js/load-public-courses.js
@@ -3,7 +3,7 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"}, function(eb){
 								'organizer'    : "Advisicon"
 							};
 							
-							$('currentPublicCourses').append('<p><strong>Loading events&hellip;</strong><p>')
+							$('currentPublicCourses').append('<p><strong>Loading events&hellip;</strong><p>');
 
 							eb.event_search( options, function( response ){
 								$('#currentPublicCourses').html('<div id="eb_event_header"><span class="head_title">Course</span><span class="head_location">Location</span>' +
@@ -16,3 +16,15 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"}, function(eb){
 			$(this).find('.eb_event_list_details').toggleClass('hidden');
 		});
 	});
+
+// Add GoToTraining courses
+$.get('https://api.citrixonline.com/G2T/rest/organizers/2221055618378781957/trainings', 
+	{
+		'Content-type': 'application/json',
+		'Accept': 'application/json',
+		'Authorization': 'OAuth oauth_token=b149f7276941cdad5fadbdc55911c111'
+	},
+	function(data){
+		$('#onlineCourses').html('<p style="color:red;">Success!</p>');
+		$('#onlineCourses').append(data);
+	}, 'json');
