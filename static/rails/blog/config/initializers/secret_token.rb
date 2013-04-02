37d7b8118d06d90cd5ae9ace60e0eb01c7dace1b
Jeff Jacobson-Swartfager <jeffja@gmail.com>
2013-04-02 16:56:23 -0700

    remove rails folder and contents

--- static/rails/blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '980f86a1def2f715b75ef7444301a33ffd57123aea222e3dd34340c0f937562ab27ab38a6e03a344e589fb888a9f8e23dfdee27b56d235feb14f695fb58f362a'
