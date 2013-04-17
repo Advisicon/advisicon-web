c75526ccba66a488e2b9286c7bf091a8caba501f
Jeff Jacobson-Swartfager <jeff.ja@gmail.com>
2013-04-17 14:19:54 -0700

    convert 'about us' to haml

--- static/js/load-public-courses.js
+++ static/js/load-public-courses.js
@@ -10,20 +10,3 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"}, function(eb){
 									'<span class="head_date">Date</span></div>'+ eb.utils.eventList( response, eb.utils.eventListRow ));
 							});
 						});
-
-
-// Add GoToTraining courses
-/*$.ajax({
-	'type': 'GET',
-	'url': 'https://api.citrixonline.com/G2T/rest/organizers/2221055618378781957', 
-	'headers': { Accept: 'application/json', Content-Type: 'application/json', Authorization: '&nbsp;OAuth&nbsp;oauth_token=b149f7276941cdad5fadbdc55911c111' },
-	'success': function(data)
-		{
-			alert('works');
-			$('#onlineCourses').html('<p style="color:red;">Success!</p>');
-			$('#onlineCourses').append(data);
-		}, 
-	'error': function(xhr){
-		alert('failed');
-	},
-	'dataType': 'json'});*/
