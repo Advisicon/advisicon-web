a2463e1aa8bd854c91b12bed777952ead99af0d4
Jeff Jacobson-Swartfager <jeff.ja@gmail.com>
2013-08-19 12:22:05 -0700

    working on running side eventbrite parse through js

--- static/js/load-public-courses.js
+++ static/js/load-public-courses.js
@@ -5,10 +5,16 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"}, function(eb){
 								'event_statuses'	: 'live'
 							};
 							
-							$('classroom-learning').append('<p><strong>Loading events&hellip;</strong><p>');
+							// $('#classroom-learning').append('<p><strong>Loading events&hellip;</strong><p>');
 
 							eb.user_list_events( options, function( response ){
 								$('#classroom-learning').html('<div id="eb_event_header"><span class="head_title">Course</span><span class="head_location">Location</span>' +
 									'<span class="head_date">Date</span></div>'+ eb.utils.eventList( response, eb.utils.eventListRow ));
+							});
+
+							eb.user_list_abbr_events( options, function( response ){
+								$('.rPopBox div ul.courses').html(
+									eb.utils.abbrEventList( response, eb.utils.abbrEventListRow )
+								);
 							});
 						});
