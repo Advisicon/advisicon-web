680785d6066c3a8a1dd982d86ff3decc18f5b43c
Jeff Jacobson-Swartfager <jeff.ja@gmail.com>
2013-08-21 09:20:43 -0700

    clean up trial request form

--- static/js/check-input.js
+++ static/js/check-input.js
@@ -1,7 +1,35 @@
 function check(input) {
-  if (input.value != $('#email_addr').value) {
+  if (input.value != document.getElementById('email_addr').value) {
     input.setCustomValidity('The two email addresses must match.');
   } else {
     input.setCustomValidity('');
   }
 }
+
+$('#submit').click(function() {
+  if ($('form')[0].checkValidity()) {
+    // form execution code
+    var fullName  = $('input#full_name').val();
+    var company   = $('input#company_name').val();
+    var emailAddr = $('input#email_addr').val();
+    var phone     = $('input#phone').val();
+
+    var dataString = 'name=' + fullName + '&company=' + company + '&email=' + emailAddr + '&phone=' + phone ;
+
+    $.ajax({
+      url:  "scripts/mail.php",
+      type: 'POST',
+      data: dataString,
+      success: function(msg){
+        disablePopupContact();
+        $('#popupMessageSent').css('visibility', 'visible');
+      },
+      error: function() {
+        alert('Bad submit');
+      }
+    });
+
+  } else {
+    // form invalid code
+  }
+});
