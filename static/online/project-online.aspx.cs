7b2f642e04f690de3ada5e89f5ead96019d1ad39
changecase <jeff.ja@gmail.com>
2013-12-10 17:05:15 -0800

    change email sender to administrator@advisicon.com

--- static/online/project-online.aspx.cs
+++ static/online/project-online.aspx.cs
@@ -98,7 +98,7 @@ public partial class _Default : System.Web.UI.Page
             //message.To.Add("Kristin.Raglione@Advisicon.com;Jeff.Jacobson@Advisicon.com;Stacey.Garner@Advisicon.com");
             message.To.Add("project@advisicon.com");
             message.Subject = "New Project Online Inquiry: " + txtFullName.Text;
-            message.From = new System.Net.Mail.MailAddress("project@advisicon.com");
+            message.From = new System.Net.Mail.MailAddress("administrator@advisicon.com");
             message.Body = html;
             System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("mail.advisicon.com");
             smtp.Send(message);
