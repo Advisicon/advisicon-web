208f9227d5acbbe608326b681b07a9e05abdaa29
changecase <jeff.ja@gmail.com>
2013-12-16 15:00:16 -0800

    add project advisor

--- helpers.rb
+++ helpers.rb
@@ -23,4 +23,23 @@ module FrankHelpers
     CGI::escapeHTML(title).gsub(/['’®]/, '\'' => '&#39;', '’' => '&rsquo;',
                                '®' => '&reg;')
   end
+
+  def hiringOrganization(org_name)
+     "<span itemprop='hiringOrganization' " +
+           "itemscope itemtype='http://schema.org/Organization' >" +
+           "<span itemprop='name'>#{org_name}</span></span>"
+  end
+  def industry(industry_name)
+     "<span itemprop='industry'>#{industry_name}</span>"
+  end
+  def location(locality, region)
+     "<span itemprop='jobLocation' itemscope " +
+           "itemtype='http://schema.org/Place'>" +
+              "<span itemprop='address' itemscope " +
+              "itemtype='http://schema.org/PostalAddress'>" +
+                "<span itemprop='addressLocality'>#{locality}</span>, " +
+                "<span itemprop='addressRegion'>#{region}</span>" +
+              "</span>" +
+            "</span>"
+  end
 end
