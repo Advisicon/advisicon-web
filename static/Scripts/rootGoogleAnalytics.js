f553bbc6f4f80cd2f1c2c7de15405665ffda7b5f
changecase <jeff.ja@gmail.com>
2014-02-24 14:07:42 -0800

    use the google provided tracking code
    fixes #95

--- static/Scripts/rootGoogleAnalytics.js
+++ static/Scripts/rootGoogleAnalytics.js
@@ -2,11 +2,9 @@
 // Subdomains like store.adviscion.com need a different tracking code
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-4534135-1', 'advisicon.com', 'auto', {'allowLinker': true}); 
-ga('require', 'linker');
-ga('linker:autoLink', ['advisicon.com', 'blog.advisicon.com', 'store.advisicon.com', 'eventbrite.advisicon.com']);
-ga('send', 'pageview');
+ga('create', 'UA-4534135-1', 'advisicon.com');
+  ga('send', 'pageview');
