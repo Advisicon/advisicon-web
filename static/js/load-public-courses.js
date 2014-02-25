27582dc1149669a25163ead6a254a7879587f08e
changecase <jeff.ja@gmail.com>
2014-02-25 11:45:15 -0800

    display no trainings at this time message when no trainings.

--- static/js/load-public-courses.js
+++ static/js/load-public-courses.js
@@ -9,8 +9,14 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"},
     // $('#classroom-learning').append('<p><strong>Loading events&hellip;</strong><p>');
 
     eb.user_list_events( options, function( response ){
-      $('#classroom-learning').html('<div id="eb_event_header"><span class="head_title">Course</span><span class="head_location">Location</span>' +
-        '<span class="head_date">Date</span></div>'+ eb.utils.eventList( response, eb.utils.eventListRow ));
+      if (response.length > 0) {
+        $('#classroom-learning').html('<div id="eb_event_header"><span class="head_title">Course</span><span class="head_location">Location</span>' +
+          '<span class="head_date">Date</span></div>'+ eb.utils.eventList( response, eb.utils.eventListRow ));
+      }
+      else
+      {
+        $('#classroom-learning').html('<p>No public trainings are available at this time.</p>');
+      }
     });
 
     eb.user_list_events( options, function( response ){
