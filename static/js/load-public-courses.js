ac312aff2f42e862681d4309441555c1da716136
changecase <jeff.ja@gmail.com>
2014-02-26 11:41:20 -0800

    update training page
    remove registration marks from M$.
    
    change if statement to display when we've got upcoming public events.

--- static/js/load-public-courses.js
+++ static/js/load-public-courses.js
@@ -9,9 +9,10 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"},
     // $('#classroom-learning').append('<p><strong>Loading events&hellip;</strong><p>');
 
     eb.user_list_events( options, function( response ){
-      if (response.length > 0) {
+      var event_list = eb.utils.eventList( response, eb.utils.eventListRow );
+      if (event_list.length > 34) { // 34 == <div class="eb_event_list">\n</div>
         $('#classroom-learning').html('<div id="eb_event_header"><span class="head_title">Course</span><span class="head_location">Location</span>' +
-          '<span class="head_date">Date</span></div>'+ eb.utils.eventList( response, eb.utils.eventListRow ));
+          '<span class="head_date">Date</span></div>'+ event_list);
       }
       else
       {
