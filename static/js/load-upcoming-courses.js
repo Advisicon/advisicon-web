9d0587ffa36987108ff688a9c997d2c776a1739b
changecase <jeff.ja@gmail.com>
2014-04-03 17:26:15 -0700

    load only the right dates, in the right order
    fucking sweet.
    
    fixes #104

--- static/js/load-upcoming-courses.js
+++ static/js/load-upcoming-courses.js
@@ -1,6 +1,6 @@
 Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"}, 
   function(eb){
-    var event_title = $('h1').text().replace(/\s+/g, "-");
+    var event_title = $('h1').text().toLowerCase();
     var options = {
       'user'           : 'jeff.jacobson@advisicon.com',
       'only_display'   : 'title,start_date,url',
@@ -8,21 +8,28 @@ Eventbrite({'app_key': "P47XBRPQTVS7YF64Z5"},
       'asc_or_desc'    : 'asc',
       'event_title'    : event_title
     };
-/*    eb.event_search( options, function( response ){
-      var dates = eb.utils.eventDates( response, eb.utils.eventDate);
-      if (dates.length > 10){ // counts characters ( 10 == <ul>\n</ul> )
-        $('section#main').append('<h2>Upcoming Public Dates</h2>' + dates);
-        var $footnotes = $('section#main .footnotes');
-        $('section#main').append( $footnotes );
-      } 
-    }); */
     eb.user_list_events( options, function( response ){
       var advisicon_events = eb.utils.eventDates( response, eb.utils.eventDate);
+      var event_array = [];
       for( i = 0; i < advisicon_events.length; i++){
-        console.log( 'TITLE: ' + advisicon_events[i].title );
-        console.log( 'URL:   ' + advisicon_events[i].url );
-        console.log( 'DATE:  ' + advisicon_events[i].date );
-        console.log( "\n---\n");
+        var adv_event = advisicon_events[i];
+        var title = adv_event.title;
+        if( title.toLowerCase() == event_title){
+          event_array.push(adv_event);
+        }
       } 
+      if( event_array.length > 0 ){
+        $('section#main').append('<h2>Upcoming Public Dates</h2>');
+        for( j = 0; j < event_array.length; j++ ){
+          var adv_event = event_array[j];
+          $('section#main').append(
+            '<ul>' +
+            '<li>' +
+            '<a href="' + adv_event.url + '" title="' + adv_event.title + '">' +
+            adv_event.date + '</a></li></ul>');
+        }
+        var $footnotes = $('section#main .footnotes');
+        $('section#main').append( $footnotes );
+      }
     });
   });
